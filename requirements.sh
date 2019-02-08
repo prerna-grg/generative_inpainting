b33738dc9218140170aafecd9788bc7c8831b9a1
Prerna Garg <33514735+prerna-grg@users.noreply.github.com>
2019-02-08 22:09:26 +0530

    flist and commands uploaded

---
+++ requirements.sh
@@ -0,0 +1,5 @@
+sudo apt install python3-pip
+pip3 install --user --upgrade tensorflow
+pip3 install git+https://github.com/JiahuiYu/neuralgym
+pip3 install opencv-contrib-python
+pip3 install pyyaml
